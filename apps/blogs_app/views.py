14e9c28d9b5d728df1dc1e4abb23403e393df8b5
Kashaer <kaiser.kaiser211@gmail.com>
2017-12-11 16:14:59 -0500

    initial commit

---
+++ apps/blogs_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    return HttpResponse("placeholder to later display all the list of blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+    
+def create(request):
+    return redirect('/')
+
+def show(request,number):
+	print number
+	return HttpResponse("placeholder to display blog {}".format(number))
+
+def edit(request,number):
+	print number
+	return HttpResponse("placeholder to edit blog {}".format(number))
+
+def destroy(request,number):
+	print number
+	return redirect('/')
+
+
